81140430b381643317536e5b5360acca1a5ca0fc
dustinturner44 <128880960+Dustinturner44@users.noreply.github.com>
2025-11-22 05:48:28 +0000

    Main T y ytt

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-const person = {
-  name: 'Gregorio Y. Zara',
+export const person = {
+  name: "Gregorio Y. Zara",
   theme: {
-    backgroundColor: 'black',
-    color: 'pink'
-  }
+    backgroundColor: "black",
+    color: "pink",
+  },
 };
-
-export default function TodoList() {
-  return (
-    <div style={person.theme}>
-      <h1>{person}'s Todos</h1>
-      <img
-        className="avatar"
-        src="https://i.imgur.com/7vQD0fPs.jpg"
-        alt="Gregorio Y. Zara"
-      />
-      <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
-      </ul>
-    </div>
-  );
-}
